a2f70b618b02aa35e7d33b45328cd6e9bf87a7b2
Hanzalah Ravat <hanzalah.ravat@gmail.com>
2020-12-28 14:38:55 +0000

    Add a module-info file

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module com.ravat.hanzalah.securechat{
+    requires javafx.base;
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+    requires com.jfoenix;
+    requires kotlin.stdlib;
+}
