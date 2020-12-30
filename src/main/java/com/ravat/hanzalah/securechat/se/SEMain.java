1e11f8ed03e8cf764335c0df32f3a8b2a8e9627e
Hanzalah Ravat <hanzalah.ravat@gmail.com>
2020-12-30 14:11:23 +0000

    Create an SELauncher which loads the correct SEContexts

---
+++ src/main/java/com/ravat/hanzalah/securechat/se/SEMain.java
@@ -0,0 +1,24 @@
+package com.ravat.hanzalah.securechat.se;
+
+import com.ravat.hanzalah.securechat.Main;
+
+import java.io.IOException;
+import java.security.KeyManagementException;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
+import java.security.cert.CertificateException;
+
+/**
+ * Derivative of Main Launcher class except that the TrustStores are loaded in and SSLContexts are set for this application
+ */
+public class SEMain extends Main {
+
+    public static void main(String[] args){
+        try {
+            new ChatTrustStore();
+        } catch (KeyManagementException | NoSuchAlgorithmException | CertificateException | KeyStoreException | IOException e) {
+            e.printStackTrace();
+        }
+        Main.main(args);
+    }
+}
